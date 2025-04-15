271978ea9d6d320532ad3eefcb6e61cba664642c
Róbert Fazekas <robert.fazekas@krm22.com>
2025-04-15 18:59:38 +0200

    check condition

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 console.log(`NODE_ENV  je ${process.env.NODE_ENV}`);
+console.log(process.env.NODE_ENV === 'production');
 
 if(process.env.NODE_ENV === 'production'){
 	module.exports = require('./prod');
